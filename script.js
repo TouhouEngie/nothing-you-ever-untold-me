0573646f33b617a21e982d4b1a280cfe8765475b
Touhou Engie <00_sling_shears@icloud.com>
2025-11-14 19:33:03 -0500

    make message bubbles more appealing

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ async function getJsonData(url, file) {
 }
 
 async function updateMessagePanel() {
-    const messages = await getJsonData(json, "unused.json");
+    const messages = await getJsonData(json, "homework/unused.json");
     const output = document.getElementById("messagearea");
     for (var i in messages) {
-        var msg = document.createElement("p");
-        msg.innerHTML = `${messages[i].text}`;
+        var msg = document.createElement("div");
+        msg.classList.add("rounded-md", "bg-sky-300", "w-9/10");
+        msg.innerHTML = `<p>${messages[i].sender}<p>${messages[i].text}<p>`;
+        if (messages[i].sender === "Nathan Lee") {
+        msg.classList.remove('bg-sky-300');
+          msg.classList.add('bg-stone-300', 'translate-x-1/9');
+        }
         output.appendChild(msg);
     }
 }
